ef6e61b0bd64cd1c1cd041c7ae1fcbc0e2b919d6
Percival Roberts <percival.roberts13@gmail.com>
2023-06-25 03:30:12 -0500

    updated methods

---
+++ 19_Method_Paramaters/Notes/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    static void name(String fname){
+        System.out.println(fname + " Bond");
+    }
+
+
+    public static void main(String[] args) {
+        name("James");
+        name("Charles");
+    }
+}
